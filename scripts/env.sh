2d1963685244605f7ba13fc8b04d68ea59ff9c4e
Maksim Rakitin <mrakitin@bnl.gov>
2019-07-16 02:57:19 -0400

    Initial set of scripts and services

---
+++ scripts/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PYTHONPATH=/home/expdev/SRW/env/work/srw_python:$PYTHONPATH
+export SIREPO_AUTH_METHODS=guest
+export SIREPO_CELERY_TASKS_CELERYD_CONCURRENCY=9
+export SIREPO_FEATURE_CONFIG_SIM_TYPES=srw
+export SIREPO_MPI_CORES=8
+export SIREPO_SERVER_DB_DIR=/home/expdev/sirepo/run
+export SIREPO_SERVER_JOB_QUEUE=Celery
---
+++ scripts/rabbitmq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+env_file=$(dirname $0)/env.sh
+echo -e "env file: $env_file"
+if [ -f "$env_file" ]; then
+    echo -e "sourcing env file: $env_file"
+    . $env_file
+fi
+
+. /opt/conda/etc/profile.d/conda.sh
+conda activate sirepo
+
+sirepo service rabbitmq
